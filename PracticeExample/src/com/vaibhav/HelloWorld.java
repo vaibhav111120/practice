487c8853ae4242a4ba5307edb48582c20c5d6830
vaibhav111120 <149307549+vaibhav111120@users.noreply.github.com>
2023-10-29 18:33:20 +0530

    sub code
    sub code added

--- PracticeExample/src/com/vaibhav/HelloWorld.java
+++ PracticeExample/src/com/vaibhav/HelloWorld.java
@@ -6,10 +6,14 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Welcome to github");
 		add(2,3);
+		sub(5,4);
 	}
 	
 	public static void add(int a, int b) {
 		System.out.println(String.format("Sum= %s", (a+b)));
+	}
+	public static void sub(int x,int y) {
+		System.out.println(String.format("sub =%s",(x-y)));
 	}
 	
 }
