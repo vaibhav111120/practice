32ce28569ae97429eca2f341b45810668503621b
vaibhav111120 <149307549+vaibhav111120@users.noreply.github.com>
2023-10-29 18:23:28 +0530

    Add addition code
    Add addition code

--- PracticeExample/src/com/vaibhav/HelloWorld.java
+++ PracticeExample/src/com/vaibhav/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	
 	public static void main(String args[]) {
 		System.out.println("Welcome to github");
+		add(2,3);
+	}
+	
+	public static void add(int a, int b) {
+		System.out.println(String.format("Sum= %s", (a+b)));
 	}
 	
 }
